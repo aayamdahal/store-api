9fc46ed7ceef09052058292f51b82fa73af3e94d
Aayam <getaayam@gmail.com>
2022-03-20 12:30:42 +0545

    connected to db and set up products route

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // first thing
 require("dotenv").config();
 // async errors
-
+require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+const productRouter = require("./routes/products.js");
 const notFoundMiddleware = require("./middleware/not-found.js");
 const errorMiddleware = require("./middleware/error-handler.js");
 
@@ -18,6 +20,8 @@ app.get("/", (req, res) => {
   );
 });
 
+app.use("/api/v1/products", productRouter);
+
 // products route
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -27,6 +31,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connectdb
+    await connectDB(process.env.MONGODB_URI);
     app.listen(port, () => {
       console.log(`server spinning on ${port}`);
     });
