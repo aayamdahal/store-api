880ab5f94cf1795578274a82e651535684459d94
Aayam <getaayam@gmail.com>
2022-03-21 11:34:26 +0545

    implemented search filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,29 @@
+const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing package");
-  res.status(200).json({ msg: "Products testing route" });
+  const search = "aa";
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products  route" });
+  const { featured, company, name, search } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  console.log(req.query.name);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
